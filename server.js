0ac39f36f407d7d34c8ee603a006275192418786
Mac <mac@Han-Jin.local>
2022-12-19 16:19:18 +0900

    각 페이지 연결

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-const express = require('express')
-const app = express()
+const express = require("express")
 const nunjucks = require('nunjucks')
+const app = express()
 
-app.set('view engine', 'html')
-nunjucks.configure('views', {
-    express: app,
+app.set("view engine", "html")
+nunjucks.configure("views", {
+    express: app
 })
 
+app.use(express.static("public"))
 
 app.get('/', (req, res) => {
     const { name } = req.query
     res.render("index.html", { name })
+})
+
+app.get('/list', (req, res) => {
+    res.render("list.html")
+})
+
+app.get('/write', (req, res) => {
+    res.render("write.html")
+})
+
+app.get('/view', (req, res) => {
+    res.render('view.html')
 })
 
 app.listen(3000, (req, res) => {
