31b65ef965e376524968fe7fa053a20fcb2c5b84
Mac <mac@Han-Jin.local>
2022-12-19 15:00:36 +0900

    동적 페이지 완성

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
+const nunjucks = require('nunjucks')
 
-app.get('/', (req, res) => {
-    res.send("Hi!")
+app.set('view engine', 'html')
+nunjucks.configure('views', {
+    express: app,
 })
 
 
-app.listen(3000, () => {
-    console.log("서버열림!")
+app.get('/', (req, res) => {
+    const { name } = req.query
+    res.render("index.html", { name })
+})
+
+app.listen(3000, (req, res) => {
+    console.log("hi")
 })
