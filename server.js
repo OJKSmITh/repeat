6c4980443e2dede9bc4bb05e923505abb7622b57
Mac <mac@Han-Jin.local>
2022-12-19 17:26:29 +0900

    1차 작은 r만들기

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ nunjucks.configure("views", {
 })
 
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: false }))
+
+const list = [{ name: "테스트", subject: "테스트", content: "테스트" }]
 
 app.get('/', (req, res) => {
     const { name } = req.query
@@ -22,8 +25,15 @@ app.get('/write', (req, res) => {
     res.render("write.html")
 })
 
+app.post('/write', (req, res) => {
+    const { subject, name, content } = req.body
+    list.push({ subject, content, name })
+    res.redirect(`/view?index=${list.length - 1}`)
+})
+
 app.get('/view', (req, res) => {
-    res.render('view.html')
+    const { index } = req.query
+    res.render('view.html', { name: list[index].name, content: list[index].content, subject: list[index].subject })
 })
 
 app.listen(3000, (req, res) => {
